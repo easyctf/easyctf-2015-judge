baf2aae68ac9ad4d771cb6ef8ed2b45dd66ba25a
David Hou <david.hou314@gmail.com>
2015-11-02 22:24:57 -0800

    Initial Commit

---
+++ run_judge.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+BASE_DIR=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
+cd ${BASE_DIR}
+
+git reset --hard
+git pull
+
+wait_for_exit(){
+    for pid in "$@"; do
+        while kill -0 "$pid"; do
+            sleep 0.5
+        done
+    done
+}
+
+if [ -e judge.pid ]
+then
+    prevpid=$(cat judge.pid)
+    if [ -n ${prevpid} && kill -0 ${prevpid} ]
+    then
+        kill ${prevpid}
+        wait_for_exit ${prevpid}
+    fi
+fi
+if [ -e db_setup.sh ]
+then
+    . db_setup.sh
+else
+    echo "db_setup.sh missing"
+fi
+nohup python3 main.py &
+echo -n $! > judge.pid
